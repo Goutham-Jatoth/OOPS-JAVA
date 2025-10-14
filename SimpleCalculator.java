fb6c27238f06aad3db90bd35cd906281e2cbd566
Goutham Jatoth <jatothugoutham7@gmail.com>
2025-10-14 11:35:23 +0530

    SimpleCalculator.java

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -69,3 +69,103 @@ public class SimpleCalculator extends JFrame implements ActionListener {
         new SimpleCalculator();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class CalculatorGUI extends JFrame implements ActionListener {
+    JTextField display;
+    int v1, v2, result;
+    char op;
+
+    public CalculatorGUI() {
+        setTitle("Calculator");
+        setSize(300, 400);
+        setLayout(new GridLayout(5, 4, 5, 5));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        display = new JTextField();
+        display.setEditable(false);
+        add(display);
+
+        String[] buttons = {
+            "7","8","9","/",
+            "4","5","6","*",
+            "1","2","3","-",
+            "0","%","=","+",
+            "Clear"
+        };
+
+        for (String text : buttons) {
+            JButton b = new JButton(text);
+            b.addActionListener(this);
+            add(b);
+        }
+
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String str = e.getActionCommand();
+
+        // If digit, append to display
+        if (str.matches("\\d")) {
+            display.setText(display.getText() + str);
+        }
+        // If operator, store first value and operator
+        else if ("+-*/%".contains(str)) {
+            v1 = Integer.parseInt(display.getText());
+            op = str.charAt(0);
+            display.setText("");
+        }
+        // Equals button
+        else if (str.equals("=")) {
+            v2 = Integer.parseInt(display.getText());
+            switch (op) {
+                case '+': result = v1 + v2; break;
+                case '-': result = v1 - v2; break;
+                case '*': result = v1 * v2; break;
+                case '/': result = v1 / v2; break;
+                case '%': result = v1 % v2; break;
+            }
+            display.setText(String.valueOf(result));
+        }
+        // Clear button
+        else if (str.equals("Clear")) {
+            display.setText("");
+        }
+    }
+
+    public static void main(String[] args) {
+        new CalculatorGUI();
+    }
+}
